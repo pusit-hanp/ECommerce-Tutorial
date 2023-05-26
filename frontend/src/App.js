796de2c91e0dac9a1d4a23d6f24a313380d1e0da
pusit-hanp <pusit.hanp@gmail.com>
2023-05-26 17:47:54 -0600

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">AppName</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
